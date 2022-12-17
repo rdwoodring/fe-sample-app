0e9b8eb9d49aef90891f8cd1eb2bd86ded5799a5
Ryan Woodring <rdwoodring@gmail.com>
2022-12-16 21:19:54 -0500

    challenge 3 - adds tests that verify the onclick is properly called when the button is clicked

--- src/components/InstructionsBar/InstructionsBar.test.tsx
+++ src/components/InstructionsBar/InstructionsBar.test.tsx
@@ -1,16 +1,25 @@
+import userEvent from '@testing-library/user-event';
+
 import InstructionsBar from './InstructionsBar';
 import { renderWithProviders } from '../../utils/test';
 
 describe('InstructionsBar', () => {
-  const defaultProps = {
-    onClick: jest.fn(),
-  };
+    const defaultProps = {
+        onClick: jest.fn(),
+    };
 
-  it('should render a "View challenges" button', () => {
-    const { getByText } = renderWithProviders(<InstructionsBar {...defaultProps} />);
-    expect(getByText('View challenges')).toBeInTheDocument();
-  });
+    it('should render a "View challenges" button', () => {
+        const { getByText } = renderWithProviders(<InstructionsBar {...defaultProps} />);
+        expect(getByText('View challenges')).toBeInTheDocument();
+    });
 
-  // TODO: Challenge 3
-  it('should call the onClick prop when the button is clicked', () => {});
+    it('should call the onClick prop when the button is clicked', () => {
+        const {
+            getByText
+        } = renderWithProviders(<InstructionsBar {...defaultProps} />);
+
+        userEvent.click(getByText('View challenges'));
+
+        expect(defaultProps.onClick).toHaveBeenCalled();
+    });
 });
