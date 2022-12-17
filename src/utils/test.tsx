4b3526637ffdb15ae041c6addd8d831077d09039
Ryan Woodring <rdwoodring@gmail.com>
2022-12-16 21:06:22 -0500

    challenge 1 - update the test utils to swap from browser router to memory router to enable better testability where routing is concerned. expose a third optional parameter to allow clients of the test util to set options on the memory router

--- src/utils/test.tsx
+++ src/utils/test.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 export const renderWithProviders = (
   ui: ReactElement,
-  opts: Partial<Parameters<typeof render>[1]> = {}
-) => render(<BrowserRouter>{ui}</BrowserRouter>, opts);
+  opts: Partial<Parameters<typeof render>[1]> = {},
+  routerOpts: React.ComponentProps<typeof MemoryRouter> = {}
+) => render(<MemoryRouter {...routerOpts}>{ui}</MemoryRouter>, opts);
