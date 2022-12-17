3256ec35ae1be77afd902a3b9fe55622b2abb417
Ryan Woodring <rdwoodring@gmail.com>
2022-12-16 21:07:48 -0500

    challenge 1 - refactors the NavBar component to do some light clean up on how elements are rendered, giving them names and breaking up the rendering. Breaks the class and styling out of the link to better accomodate styling of the active class applied by react router. swap from link ro NavLink to support the active class. Add the aria-current attribute. Add tests to verify the aria-current attribute.

--- src/components/NavBar/NavBar.test.tsx
+++ src/components/NavBar/NavBar.test.tsx
@@ -2,22 +2,38 @@ import NavBar from './NavBar';
 import { renderWithProviders } from '../../utils/test';
 
 describe('NavBar', () => {
-  const defaultProps = {
-    links: [
-      { text: 'Link1', href: '/link1' },
-      { text: 'Link2', href: '/link2' },
-      { text: 'Link3', href: '/link3' },
-    ],
-  };
+    const defaultProps = {
+        links: [
+            { text: 'Link1', href: '/link1' },
+            { text: 'Link2', href: '/link2' },
+            { text: 'Link3', href: '/link3' },
+        ],
+    };
 
-  it('should render NavBar links', () => {
-    const { getByText } = renderWithProviders(<NavBar {...defaultProps} />);
+    it('should render NavBar links', () => {
+        const { getByText } = renderWithProviders(<NavBar {...defaultProps} />);
 
-    expect(getByText('Link1')).toBeInTheDocument();
-    expect(getByText('Link2')).toBeInTheDocument();
-    expect(getByText('Link3')).toBeInTheDocument();
-  });
+        expect(getByText('Link1')).toBeInTheDocument();
+        expect(getByText('Link2')).toBeInTheDocument();
+        expect(getByText('Link3')).toBeInTheDocument();
+    });
 
-  // TODO: Challenge 2
-  it('should render an `href` attribute for each link', () => {});
+    it('should render an aria-current attribute for the active link', () => {
+        const { getByText } = renderWithProviders(<NavBar {...defaultProps} />, {}, {
+            initialEntries: ['/link1']
+        });
+
+        expect(getByText('Link1').getAttribute('aria-current')).toBe('page');
+    });
+
+    it('should not render an aria-current attribute for non-active links', () => {
+        const { getByText } = renderWithProviders(<NavBar {...defaultProps} />, {}, {
+            initialEntries: ['/link1']
+        });
+
+        expect(getByText('Link2').getAttribute('aria-current')).toBeFalsy();
+    });
+
+    // TODO: Challenge 2
+    it('should render an `href` attribute for each link', () => { });
 });
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,54 +1,85 @@
-import { Link, Box } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
-type TNavBar = {
-  links: {
-    text: string;
-    href: string;
-    'data-testid'?: string;
-  }[];
+import { 
+    Link, 
+    Box 
+} from '@mui/material';
+
+import { 
+    NavLink as RouterLink 
+} from 'react-router-dom';
+
+type Props = {
+    links: {
+        text: string;
+        href: string;
+        'data-testid'?: string;
+    }[];
 };
 
-function NavBar({ links }: TNavBar) {
-  return (
-    <Box
-      component="aside"
-      sx={{
-        background: '#0c2975',
-        padding: '16px',
-        width: '200px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <Link
-        component={RouterLink}
-        to="/"
-        sx={{ cursor: 'pointer', marginBottom: '80px', marginTop: '40px' }}
-      >
-        <img src="/surelogo.svg" alt="logo"></img>
-      </Link>
+const navLinkStyles = {
+    cursor: 'pointer',
+    padding: '4px 8px',
+    '&:not(:last-of-type)': {
+        marginBottom: '16px',
+    },
+    '&.active': {
+        fontWeight: '600',
+        backgroundColor: '#000029'
+    }
+};
 
-      {links.map(({ text, href, 'data-testid': dataTestId }) => (
-        <Link
-          component={RouterLink}
-          key={href}
-          to={href}
-          color="#fff"
-          underline="hover"
-          sx={{
-            cursor: 'pointer',
-            '&:not(:last-of-type)': {
-              marginBottom: '16px',
-            },
-          }}
-          data-testid={dataTestId}
+function NavBar(props: Props) {
+    const {
+        links
+    } = props,
+        linksMarkup = links.map((link) => {
+            const {
+                text,
+                href,
+                'data-testid': testId
+            } = link;
+
+            return (
+                <Link
+                    aria-current="page"
+                    component={RouterLink}
+                    key={href}
+                    to={href}
+                    color="#fff"
+                    underline="hover"
+                    sx={navLinkStyles}
+                    data-testid={testId}
+                >
+                    {text}
+                </Link>
+            );
+        }),
+        logoMarkup = (
+            <Link
+                component={RouterLink}
+                to="/"
+                sx={{ cursor: 'pointer', marginBottom: '80px', marginTop: '40px' }}
+            >
+                <img src="/surelogo.svg" alt="logo"></img>
+            </Link>
+        );
+
+    return (
+        <Box
+            component="aside"
+            sx={{
+                background: '#0c2975',
+                padding: '16px',
+                width: '200px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
         >
-          {text}
-        </Link>
-      ))}
-    </Box>
-  );
+            {logoMarkup}
+
+            {linksMarkup}
+        </Box>
+    );
 }
 
 export default NavBar;
