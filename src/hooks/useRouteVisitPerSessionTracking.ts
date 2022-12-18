a521fec625607c277291ed999f225b10a53c6ed3
Ryan Woodring <rdwoodring@gmail.com>
2022-12-18 10:38:20 -0500

    challenge 5 - forgot to add the hook doing the history tracking. adds hook that tracks what paths are visited and increments keys in a hashmap tracking visits to each route during a session. since redux is volatile, refreshing or navigating away will reset the redux state

---
+++ src/hooks/useRouteVisitPerSessionTracking.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import {
+    useLocation
+} from 'react-router-dom';
+
+import {
+    useAppDispatch
+} from './useAppDispatch';
+
+import {
+    useAppSelector
+} from './useAppSelector';
+
+import {
+    visitRoute
+} from '../redux/reducers/visitedRoutes';
+
+function useRouteVisitePerSessionTracking() {
+    const {
+        pathname
+    } = useLocation(),
+    dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(visitRoute(pathname));
+    }, [
+        dispatch,
+        pathname
+    ]);
+}
+
+export default useRouteVisitePerSessionTracking;
