473019724457bb45bbc608c16af9a7be383b97ea
Ryan Woodring <rdwoodring@gmail.com>
2022-12-18 10:35:46 -0500

    challenged 5 - reorganize app and index to move the router responsibilities out to index.tsx with other providers like the redux store provider, paving the way for global hooks needing access to history or location in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import TableDemoView from './components/TableDemoView';
 import HomeView from './components/HomeView';
-import RedirectView from './components/RedirectView';
+import PolicyHoldersView from './components/PolicyHoldersView';
 import YouCanDoItView from './components/YouCanDoItView';
+import RedirectView from './components/RedirectView';
+
 import Layout from './components/Layout';
-import { useState } from 'react';
 import Modal from './components/Modal';
 import Instructions from './components/Instructions';
 
-function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+import useRouteVisitePerSessionTracking from './hooks/useRouteVisitPerSessionTracking';
 
-  return (
-    <BrowserRouter>
-      <Layout onFooterClick={() => setIsModalOpen(true)}>
-        <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/table" element={<TableDemoView />} />
-          <Route path="/you-can-do-it" element={<YouCanDoItView />} />
-          <Route path="*" element={<RedirectView />} />
-        </Routes>
-      </Layout>
-      <Modal
-        isOpen={isModalOpen}
-        handleClose={() => setIsModalOpen(false)}
-        title="Sure Technical Challenge"
-      >
-        <Instructions />
-      </Modal>
-    </BrowserRouter>
-  );
+function App() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    useRouteVisitePerSessionTracking();
+    return (
+        <>
+            <Layout onFooterClick={() => setIsModalOpen(true)}>
+                <Routes>
+                    <Route path="/" element={<HomeView />} />
+                    <Route path="/table" element={<TableDemoView />} />
+                    <Route path="/you-can-do-it" element={<YouCanDoItView />} />
+                    <Route path="/policyholders" element={<PolicyHoldersView />} />
+                    <Route path="*" element={<RedirectView />} />
+                </Routes>
+            </Layout>
+            <Modal
+                isOpen={isModalOpen}
+                handleClose={() => setIsModalOpen(false)}
+                title="Sure Technical Challenge"
+            >
+                <Instructions />
+            </Modal>
+        </>
+    );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -11,11 +11,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import {store} from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
