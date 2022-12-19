185227ad51d12a1f36d13056166ee45ae340ef31
Ryan Woodring <rdwoodring@gmail.com>
2022-12-18 22:01:15 -0500

    challenge 6 - adds a utililty type for unwrapping a record type

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -2,6 +2,11 @@ type UnwrapArray<T> = T extends Array<any> ?
     T[number] :
     never;
 
+type UnwrapRecord<T> = T extends Record<string, unknown> ?
+    T[string] :
+    never;
+
 export type {
-    UnwrapArray
+    UnwrapArray,
+    UnwrapRecord
 };
