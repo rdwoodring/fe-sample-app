0144a2715bb8a00572817bad47a82af830df9820
Ryan Woodring <rdwoodring@gmail.com>
2022-12-18 21:22:13 -0500

    challenge 6 - use the PolicyHolder type to have better type support for the getPolicyHolders api function

--- src/apis/policyholders/policyHolderApi.ts
+++ src/apis/policyholders/policyHolderApi.ts
@@ -1,11 +1,17 @@
 import axios from 'axios';
 
+import {
+    PolicyHolder
+} from '../../redux/reducers/policyHolders';
+
 const baseUrlRaw = 'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/';
 
 function getPolicyHolders() {
     const url = new URL('api/policyholders', baseUrlRaw);
 
-    return axios.get(url.href);
+    return axios.get<{
+        policyHolders: PolicyHolder[]
+    }>(url.href);
 }
 
 export {
