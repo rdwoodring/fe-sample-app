cbd091e7112984bad8daaf74b16f44560b768ae4
Ryan Woodring <rdwoodring@gmail.com>
2022-12-18 22:01:43 -0500

    challenge 6 - render info about the policy holder to the screen

--- src/components/PolicyHoldersView/PolicyHoldersView.spec.tsx
+++ src/components/PolicyHoldersView/PolicyHoldersView.spec.tsx
@@ -5,6 +5,10 @@ import PolicyHoldersView, {
     Props
 } from './PolicyHoldersView';
 
+import {
+    screen
+} from '@testing-library/react'
+
 let defaultProps: Props,
     reduxData: any;
 
@@ -17,7 +21,24 @@ beforeEach(() => {
 
     jest.spyOn(policyHoldersApi, 'getPolicyHolders').mockImplementation(() => {
         return Promise.resolve({
-            data: []
+            data: {
+                policyHolders: [
+                    {
+                        name: "Mrs. Holder",
+                        age: 29,
+                        address: {
+                            line1: "123 Lane Ave",
+                            line2: "3H",
+                            city: "Santa Monica",
+                            state: "CA",
+                            postalCode: "90405",
+                        },
+                        phoneNumber: "1-989-989-9898",
+                        isPrimary: true,
+                    }
+                ]
+            }
+             
         } as AxiosResponse);
     })
 });
@@ -55,7 +76,7 @@ describe('when the component mounts', () => {
             };
         });
 
-        it('should not call getPolicyHolders', () => {
+        it('should call getPolicyHolders', () => {
             renderWithProviders(
                 <PolicyHoldersView {...defaultProps} />,
                 {},
@@ -67,5 +88,42 @@ describe('when the component mounts', () => {
         
             expect(policyHoldersApi.getPolicyHolders).toHaveBeenCalled();
         });
+
+        it('should render a loading state while waiting', () => {
+            renderWithProviders(
+                <PolicyHoldersView {...defaultProps} />,
+                {},
+                {
+                    initialEntries: ['/policyholders']
+                },
+                reduxData
+            );
+
+            expect(screen.getByText('Loading...')).toBeTruthy();
+        })
+
+        it('should render the results', async () => {
+            renderWithProviders(
+                <PolicyHoldersView {...defaultProps} />,
+                {},
+                {
+                    initialEntries: ['/policyholders']
+                },
+                reduxData
+            );
+
+            await screen.findAllByText('Mrs. Holder');
+
+            expect(screen.getByText('Name')).toBeTruthy();
+            expect(screen.getByText('Age')).toBeTruthy();
+            expect(screen.getByText('Address')).toBeTruthy();
+            expect(screen.getByText('Phone number')).toBeTruthy();
+            expect(screen.getByText('Primary policyholder?')).toBeTruthy();
+
+            expect(screen.getByText('29')).toBeTruthy();
+            expect(screen.getByText('123 Lane Ave, 3H, Santa Monica, CA 90405')).toBeTruthy();
+            expect(screen.getByText('1-989-989-9898')).toBeTruthy();
+            expect(screen.getByText('true')).toBeTruthy();
+        })
     });
 });
--- src/components/PolicyHoldersView/PolicyHoldersView.tsx
+++ src/components/PolicyHoldersView/PolicyHoldersView.tsx
@@ -1,15 +1,89 @@
-import { getPolicyHolders } from '../../apis/policyholders/policyHolderApi';
+import usePolicyHolderApi from '../../apis/policyholders/usePolicyHolderApi';
+import { useAppSelector } from '../../hooks/useAppSelector';
+import useLoadUntilDataExists from '../../hooks/useLoadingUntilDataExists';
 
 import useOnInitialRouteMount from '../../hooks/useOnInitialRouteMount';
+import { UnwrapArray, UnwrapRecord } from '../../utils/types';
+
+import {Box} from '@mui/material';
+import InfoTable from '../InfoTable';
 
 type Props = {
 
 }
 
+const fieldLabelPrettyTranslation = {
+    'name': 'Name',
+    'age': 'Age',
+    'flattenedAddress': 'Address',
+    'phoneNumber': 'Phone number',
+    'isPrimary': 'Primary policyholder?'
+}
+
 function PolicyHoldersView(props: Props) {
+    const {
+        getPolicyHolders
+    } = usePolicyHolderApi(),
+    policyHolders = useAppSelector(state => state.policyHolders),
+    loading = useLoadUntilDataExists(policyHolders);
+
     useOnInitialRouteMount(getPolicyHolders);
 
-    return null;
+    
+    function generatePolicyHolderInfoTables() {
+        function generatePolicyHolderInfoTable(policyHolder: UnwrapRecord<typeof policyHolders>) {
+            const {
+                address: {
+                    line1,
+                    line2,
+                    city,
+                    state,
+                    postalCode
+                }
+            } = policyHolder,
+            policyHolderWithFlattenedAddress: typeof policyHolder & {
+                flattenedAddress: string
+            } = {
+                ...policyHolder,
+                flattenedAddress: `${line1}, ${line2}, ${city}, ${state} ${postalCode}`
+            },
+            policyHolderFields: Array<Exclude<keyof typeof policyHolderWithFlattenedAddress, 'address'>> = [
+                'name',
+                'age',
+                'flattenedAddress',
+                'phoneNumber',
+                'isPrimary'
+            ],
+            rows = policyHolderFields.map(fieldname => {
+                return {
+                    key: fieldLabelPrettyTranslation[fieldname],
+                    value: String(policyHolderWithFlattenedAddress[fieldname])
+                }
+            });
+
+            return <InfoTable header={policyHolder.name} rows={rows} />
+        }
+        
+        return Object.values(policyHolders).map(generatePolicyHolderInfoTable)
+    }
+
+    let markup;
+
+    if (loading) {
+        markup = (
+            <div>
+                Loading...
+            </div>
+        )
+    } else {
+        markup = generatePolicyHolderInfoTables();
+    }
+
+    return (
+        <Box sx={{textAlign: 'center'}}>
+            {markup}
+        </Box>
+    );
 }
 
 export default PolicyHoldersView;
