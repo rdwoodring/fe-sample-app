dd86d156fe683cc4ff9991996b930867df49178c
Ryan Woodring <rdwoodring@gmail.com>
2022-12-19 21:16:48 -0500

    challenge 10 - update the cypress happy path test

--- cypress/integration/happy_path.spec.ts
+++ cypress/integration/happy_path.spec.ts
@@ -7,6 +7,25 @@ describe('happy path', () => {
     cy.getTestEl('you_go_link').should('be.visible');
     cy.getTestEl('policyholders_link').should('be.visible');
 
+    cy.intercept('GET', 'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders')
+      .as('post');
+
+    cy.getTestEl('policyholders_link').click();
+
+    cy.contains('body', 'Mrs. Holder');
+
+    cy.get('@post')
+      .its('response')
+      .then((res) => {
+        expect(res.statusCode).to.equal(200);
+      });
+
+    cy.get('body').should('contain', 'Mrs. Holder');
+    cy.get('body').should('contain', '29');
+    cy.get('body').should('contain', '123 Lane Ave, 3H, Santa Monica, CA 90405');
+    cy.get('body').should('contain', '1-989-989-9898');
+    cy.get('body').should('contain', 'true');
+
     /**
      * TODO: Challenge 10 - Update this test
      * - Click the Policyholders sidebar link
